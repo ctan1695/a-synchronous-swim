9704eb8d0e27a9f3c90ce9a6e357a784a11e76c1
Tim Anderson <tdanderson805@gmail.com>
2020-12-16 17:49:35 -0800

    more solo work

--- client/js/randomSwimmer.js
+++ client/js/randomSwimmer.js
@@ -2,7 +2,21 @@
 
 console.log('randomSwimmer envoked');
 
-// var success = () => {};
+async function waitTime(time) {
+  return new Promise((resolve) => {
+     setTimeout(resolve, time);
+  });
+}
+async function messageTyper() {
+  for (let i = 0; i < 10; i++) {
+     await waitTime(2000);
+     console.log('my spaced out message')
+  }
+}
+
+
+
+// var success = (data) => {console.log(data)};
 
 // swimFetcher(success);
 
